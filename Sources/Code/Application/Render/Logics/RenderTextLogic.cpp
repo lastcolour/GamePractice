8df45b7ff7438a2da043012d5c0839b50fe7df57
lastcolour <alexzhogan@gmail.com>
2019-04-26 21:47:51 +0300

    Minor tweak

--- Sources/Code/Application/Render/Logics/RenderTextLogic.cpp
+++ Sources/Code/Application/Render/Logics/RenderTextLogic.cpp
@@ -28,7 +28,7 @@ void RenderTextLogic::createVAO() {
     glBindBuffer(GL_ARRAY_BUFFER, vboId);
     glBufferData(GL_ARRAY_BUFFER, sizeof(Vec4) * 6, nullptr, GL_DYNAMIC_DRAW);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(Vec4), 0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, sizeof(Vec4), static_cast<void*>(0));
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 }
